4ab5f46d7ae7296c46a67d2eed94059e068e3afd
Kevin Rutherford <kevin@rutherford-software.com>
2022-07-27 14:45:13 +0100

    Add the next failing test

--- src/checkout.test.js
+++ src/checkout.test.js
@@ -4,6 +4,10 @@ describe('Back to the checkout', () => {
       const price = 0
       expect(price).toBe(0)
     })
+
+    it('is 50 for a basket containing an A', () => {
+      expect(price).toBe(50)
+    })
   })
 })
 
