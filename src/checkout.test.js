fc93f2ccda3dfa16dbfcaf23c8c584f7e8c7e74d
Kevin Rutherford <kevin@rutherford-software.com>
2022-07-28 10:01:07 +0100

    Make the tests more similar shapes

--- src/checkout.test.js
+++ src/checkout.test.js
@@ -2,12 +2,13 @@ describe('Back to the checkout', () => {
   describe('Total price', () => {
     it('is zero before any items are scanned', () => {
       const price = 0
+      const itemsScanned = []
       expect(price).toBe(0)
     })
 
     it('is 50 for a basket containing an A', () => {
       let price = 0
-      const itemsScanned = 'A'
+      const itemsScanned = ['A']
       price = 50
       expect(price).toBe(50)
     })
