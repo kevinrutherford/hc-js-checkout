84510490331bf385486f86dae4b41045116fd7ec
Kevin Rutherford <kevin@rutherford-software.com>
2022-07-28 10:30:44 +0100

    Tell the same story in both tests

--- src/checkout.test.js
+++ src/checkout.test.js
@@ -1,16 +1,21 @@
 describe('Back to the checkout', () => {
   describe('Total price', () => {
     it('is zero before any items are scanned', () => {
-      const price = 0
+      let currentPrice = 0
       const itemsScanned = []
-      expect(price).toBe(0)
+      currentPrice = itemsScanned.length > 0
+        ? currentPrice + 50
+        : currentPrice
+      expect(currentPrice).toBe(0)
     })
 
     it('is 50 for a basket containing an A', () => {
-      let price = 0
+      let currentPrice = 0
       const itemsScanned = ['A']
-      price = 50
-      expect(price).toBe(50)
+      currentPrice = itemsScanned.length > 0
+        ? currentPrice + 50
+        : currentPrice
+      expect(currentPrice).toBe(50)
     })
   })
 })
