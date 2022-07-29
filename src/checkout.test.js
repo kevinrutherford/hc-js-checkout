df630c5049d13d66e3c8e6cce2d4d87ddb06c155
Kevin Rutherford <kevin@rutherford-software.com>
2022-07-29 15:45:38 +0100

    Replace the tests with a table of cases

--- src/checkout.test.js
+++ src/checkout.test.js
@@ -1,4 +1,4 @@
-const scan = (itemsScanned) => {
+const totalPriceOf = (itemsScanned) => {
   let currentPrice = 0
   currentPrice = itemsScanned.length > 0
     ? currentPrice + 50
@@ -8,14 +8,11 @@ const scan = (itemsScanned) => {
 
 describe('Back to the checkout', () => {
   describe('Total price', () => {
-    it('is zero before any items are scanned', () => {
-      let totalPrice = scan([])
-      expect(totalPrice).toBe(0)
-    })
-
-    it('is 50 for a basket containing an A', () => {
-      let totalPrice = scan(['A'])
-      expect(totalPrice).toBe(50)
+    it.each([
+      [  0, []    ],
+      [ 50, ['A'] ],
+    ])('is %d for a basket containing %s', (expectedTotalPrice, items) => {
+      expect(totalPriceOf(items)).toBe(expectedTotalPrice)
     })
   })
 })
