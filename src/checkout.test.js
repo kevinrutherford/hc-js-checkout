c9d68898ab4d13baf2b6aaa99789983d4281637c
Kevin Rutherford <kevin@rutherford-software.com>
2022-07-29 15:15:08 +0100

    Rename function to tell a clearer story

--- src/checkout.test.js
+++ src/checkout.test.js
@@ -1,4 +1,4 @@
-const checkout = (itemsScanned) => {
+const scan = (itemsScanned) => {
   let currentPrice = 0
   currentPrice = itemsScanned.length > 0
     ? currentPrice + 50
@@ -9,12 +9,12 @@ const checkout = (itemsScanned) => {
 describe('Back to the checkout', () => {
   describe('Total price', () => {
     it('is zero before any items are scanned', () => {
-      let totalPrice = checkout([])
+      let totalPrice = scan([])
       expect(totalPrice).toBe(0)
     })
 
     it('is 50 for a basket containing an A', () => {
-      let totalPrice = checkout(['A'])
+      let totalPrice = scan(['A'])
       expect(totalPrice).toBe(50)
     })
   })
