74e450d532e8a68956ebf232c9bb36247c4f3ba7
Kevin Rutherford <kevin@rutherford-software.com>
2022-07-29 15:01:42 +0100

    Rename variable to match the domain

--- src/checkout.test.js
+++ src/checkout.test.js
@@ -9,13 +9,13 @@ const checkout = (itemsScanned) => {
 describe('Back to the checkout', () => {
   describe('Total price', () => {
     it('is zero before any items are scanned', () => {
-      let currentPrice = checkout([])
-      expect(currentPrice).toBe(0)
+      let totalPrice = checkout([])
+      expect(totalPrice).toBe(0)
     })
 
     it('is 50 for a basket containing an A', () => {
-      let currentPrice = checkout(['A'])
-      expect(currentPrice).toBe(50)
+      let totalPrice = checkout(['A'])
+      expect(totalPrice).toBe(50)
     })
   })
 })
