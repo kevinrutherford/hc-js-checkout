0e9abfe17f93c380a0f99ae8920a4695fe62923a
Kevin Rutherford <kevin@rutherford-software.com>
2022-07-29 14:36:59 +0100

    Extract checkout function

--- src/checkout.test.js
+++ src/checkout.test.js
@@ -1,20 +1,20 @@
+const checkout = (itemsScanned) => {
+  let currentPrice = 0
+  currentPrice = itemsScanned.length > 0
+    ? currentPrice + 50
+    : currentPrice
+  return currentPrice
+}
+
 describe('Back to the checkout', () => {
   describe('Total price', () => {
     it('is zero before any items are scanned', () => {
-      let currentPrice = 0
-      const itemsScanned = []
-      currentPrice = itemsScanned.length > 0
-        ? currentPrice + 50
-        : currentPrice
+      let currentPrice = checkout([])
       expect(currentPrice).toBe(0)
     })
 
     it('is 50 for a basket containing an A', () => {
-      let currentPrice = 0
-      const itemsScanned = ['A']
-      currentPrice = itemsScanned.length > 0
-        ? currentPrice + 50
-        : currentPrice
+      let currentPrice = checkout(['A'])
       expect(currentPrice).toBe(50)
     })
   })
