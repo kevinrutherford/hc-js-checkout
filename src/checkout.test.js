e82503d135038ef9afe38ca572d87537b5048d61
Kevin Rutherford <kevin@rutherford-software.com>
2022-08-22 10:27:17 +0100

    calculate price for basket with A and B

--- src/checkout.test.js
+++ src/checkout.test.js
@@ -2,6 +2,8 @@ const totalPriceOf = (items) => {
   let totalPrice = 0
   if (items.length > 0)
     totalPrice = totalPrice + 50
+  if (items.length > 1)
+    totalPrice = totalPrice + 30
   return totalPrice
 }
 
@@ -10,6 +12,7 @@ describe('Back to the checkout', () => {
     it.each([
       [  0, []    ],
       [ 50, ['A'] ],
+      [ 80, ['A', 'B'] ],
     ])('is %d for a basket containing %s', (expectedTotalPrice, items) => {
       expect(totalPriceOf(items)).toBe(expectedTotalPrice)
     })
